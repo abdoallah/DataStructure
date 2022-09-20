3b8a990f51c080e918746a053382de9c306d51a9
ashahin <ashahin@pixel.com.kw>
2022-09-20 17:49:35 +0200

    Generic Stack Implemntation

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace DataStructure
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //int TopVal = 0;
+            Stack<char> s = new Stack<char>();
+            s.Push('w');
+            s.Push('w');
+            s.Push('w');
+            s.Push('w');
+            s.Push('w');
+            s.Push('w');
+            s.pop();
+            s.Print();
+            //s.GetTop(ref TopVal);
+
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DataStructure
+{
+    public class Stack<T>
+    {
+        const int   Max_Size =100;
+        public int Top { get; set; }
+
+        T [] Item = new T[Max_Size];
+
+        public Stack(int top = -1 )
+        {
+            Top = top;
+        }
+
+       public void Push (T element)
+        {
+            if (Top >= Max_Size - 1)
+                Console.WriteLine("Stack is full");
+            else 
+            Top++;
+            Item[Top] = element;
+           
+        }
+        bool IsEmpty()
+        {
+           return  Top<0;
+        }
+       public void pop()
+        {
+            if (IsEmpty())
+              Console.WriteLine("stack is empty");
+            else
+            Top--;
+            
+        }
+       public void pop(ref T Elemnt )
+        {
+            if (IsEmpty())
+                Console.WriteLine("stack is empty");
+            else
+                Elemnt= Item[Top]  ;
+                Top--;
+        }
+        public void  GetTop(ref T StackTop)
+        {
+            if (IsEmpty())
+                Console.WriteLine("stack is empty");
+            else
+                StackTop = Item[Top];
+            Console.WriteLine(StackTop);
+        }
+         public void Print()
+        {
+            if (!IsEmpty())
+            {
+                Console.Write("[");
+                for (int i = Top; i >= 0; i--)
+                {
+                    Console.Write(Item[i] + " ");
+                }
+                Console.Write("]");
+            }
+            else
+            {
+                Console.WriteLine("stack is empty");
+            }
+            
+        }
+    }
+}
